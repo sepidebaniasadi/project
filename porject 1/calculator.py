2585796195cf0b26c3a94ba26a73b09f51db7370
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-17 17:19:14 +0430

    Update calculator.py

--- porject 1/calculator.py
+++ porject 1/calculator.py
@@ -39,16 +39,16 @@ for i in range(rank):
             answer=number1%number2
             print(answer)
      if basic_or_advanced==2:
-            print("please select :"'\n''_____________''\n''1)abs''\n''2)Trighanbes''\n''3)sqrt''\n''4)even or odd''\n''5)log''\n')
+            print("please select :"'\n''_____________''\n''1)abs''\n''2)Trighanbes''\n''3)sqrt''\n''4)even or odd''\n''5)log''\n''6)kmm''\n''7)bmm''\n''8)factorial')
             select=int(float(input("select: ")))
             if select==1:
-                number=int(float(input("enter your number: ")))
+                number=float(input("enter your number: "))
                 answer=abs(number)
                 print(answer)
             elif select==2:
-                print('________________'"please select: "'\n''1)sin''\n''2)cos''\n''3)tan''\n''4)cot')
+                print('________________'"please select: "'\n''1)sin''\n''2)cos''\n''3)tan''\n''4)cot''\n''6)kmm''\n''6)bmm''\n''7)factorial')
                 select2=int(float(input("select= ")))
-                number2=int(float(input("enter your number: ")))
+                number2=float(input("enter your number: "))
                 if select2==1:
                     answer=math.sin(number2)
                     print(answer)
@@ -62,25 +62,37 @@ for i in range(rank):
                     answer=math.cot(number2)
                     print(answer)
             elif select==3:
-                number3=int(float(input("enter your number: ")))
+                number3=float(input("enter your number: "))
                 answer=math.sqrt(number2)
                 print(answer)
             elif select==4:
-                number4=int(float(input("enter number")))
+                number4=float(input("enter number"))
                 if(number4%2==0):
                     print("your number is even")
                 else:
                     print("your number is odd")
             elif select==5:
-                number5=int(float(input("enter number")))
+                number5=float(input("enter number"))
                 answer=math.log(number5)
                 print(answer)
+            elif select==6:
+                number6=float(input('enter number: '))
+                number7=float(input('enter number: '))
+                print("kmm: " , math.lcm(number6, number7))
+            elif select==7:
+                number8=float(input('enter number: '))
+                number9=float(input('enter number: '))
+                print("bmm: " , math.gcd(number8, number9))
+            elif select==8:
+                number10 = int(float(input("please Enter integer number: ")))
+                if number10 < 0:
+                    print("Sorry, factorial does not exist for negative numbers")
+                else:
+                    result = math.factorial(number10)
+                    print("The factorial of {} is: {}".format(number10, result))
      else:
         print ("wrong we dont have this number")
         break
-
-
-
 
 
 
