0af90d452f4f071f80ddd4aadbc53e1b88e2e04f
sepidebaniasadi <110469062+sepidebaniasadi@users.noreply.github.com>
2022-08-20 11:51:22 +0430

    Update calculator.py

--- porject 1/calculator.py
+++ porject 1/calculator.py
@@ -8,10 +8,10 @@ for i in range(rank):
      print(" ________________""\n""|""1)basic",space2,"|\n""|",space,"|\n""|",space,"|\n""|",space,"|\n""|""2)advanced",space3,"|\n""|""________________""|")
      basic_or_advanced=int(input("answer: "))
      if basic_or_advanced==1:
-        print("1)basic"'\n'"2)basic sequence")
+        print('='*10,'selection','='*10,'\n'"1)basic"'\n'"2)basic sequence")
         select_basic=int(input("select:"))
         if select_basic==1:
-            print("you are select basic you can yose oration"'\n''(+ or sum)''\n''(- or subtraction)''\n''(* or zarb)''\n''(** or power)''\n''(/ or split)''\n''(// or split int)''\n''(%  mod)') 
+            print("you are select basic you can use opration"'\n''(+ or sum)''\n''(- or subtraction)''\n''(* or zarb)''\n''(** or power)''\n''(/ or split)''\n''(// or split int)''\n''(%  mod)') 
             number1=float(input("Enter the number on the left side of the operation "))
             Operation=input("enter your a opration: ")
             number2=float(input("Enter the number on the right side of the operation "))
@@ -81,14 +81,14 @@ for i in range(rank):
                     continue
             
      elif basic_or_advanced==2:
-            print("please select :"'\n''_____________''\n''1)abs''\n''2)Trighanbes''\n''3)sqrt''\n''4)even or odd''\n''5)log10''\n''6)kmm''\n''7)bmm''\n''8)factorial''\n''9)Convert radians to degrees''\n''10)convert degrees to radians''\n''11)ceil''\n''12)floor''\n''13)exp''\n''14)log(x,b)''\n''15)int(x)''\n''16)hypot(x,y)''\n''17)power(x,y)')
+            print('='*10,'selection','='*10,'\n''1)abs''\n''2)Trighanbes''\n''3)sqrt''\n''4)even or odd''\n''5)log10''\n''6)kmm''\n''7)bmm''\n''8)factorial''\n''9)Convert radians to degrees''\n''10)convert degrees to radians''\n''11)ceil''\n''12)floor''\n''13)exp''\n''14)log(x,b)''\n''15)int(x)''\n''16)hypot(x,y)''\n''17)power(x,y)''\n''18)convert number')
             select=int(float(input("select: ")))
             if select==1:
                 number=float(input("enter your number: "))
                 answer=abs(number)
                 print(answer)
             elif select==2:
-                print('________________'"please select: "'\n''1)sin''\n''2)cos''\n''3)tan''\n''4)arsin''\n''5)arcos''\n''6)artan''\n''7)arsinh''\n''8)arcosh')
+                print('='*10,'selection','='*10,'\n'"please select: "'\n''1)sin''\n''2)cos''\n''3)tan''\n''4)arsin''\n''5)arcos''\n''6)artan''\n''7)arsinh''\n''8)arcosh')
                 select2=int(float(input("select= ")))
                 number2=float(input("enter your number: "))
                 if select2==1:
@@ -185,6 +185,30 @@ for i in range(rank):
                  y=float(input("please Enter  y : "))
                  answer=math.pow(x,y)
                  print(answer)
+            elif select==18:
+                print('='*10,'convert','='*10,'\n''1)bin''\n''2)hex''\n''3)oct')
+                select=int(input('select:'))
+                if select==1:
+                    num=int(input("enter num"))
+                    if num<2:
+                        print("wrong please enter number bigger than 2")
+                    elif num>2:
+                        print ('your conver is: ',bin(num))
+                elif select==2:
+                    num=int(input("enter num"))
+                    if num<16:
+                        print("wrong please enter number bigger than 16")
+                    elif num>16:
+                        print ('your conver is: ',hex(num))
+                elif select==3:
+                    num=int(input("enter num"))
+                    if num<8:
+                        print("wrong please enter number bigger than 8")
+                    elif num>8:
+                        print ('your conver is: ',oct(num))
+                else:
+                    print('we dont have your select ')
+                    break
      else:
         print ("wrong we dont have this number")
         break
